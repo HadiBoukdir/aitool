36f09cfcee9a828e07950abc2a6f4d73fc534b1a
hadi <hadi.boukdir@gmail.com>
2024-12-20 13:18:13 +0000

    Initial Commit

---
+++ app/(root)/page.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import {SignedIn, SignedOut, SignInButton, UserButton} from "@clerk/nextjs";
+
+const Home = () => {
+    return (
+        <div>
+            <p>Home</p>
+        </div>
+    );
+};
+
+export default Home;
